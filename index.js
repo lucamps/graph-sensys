ca796c9a5e87ff3dbe3b37ecc8d2ce08be5c0edf
lucamps <lucas.campos2@ufv.br>
2023-04-25 00:25:12 -0300

    configurando: bootstrap e sass funcionando

--- index.js
+++ index.js
@@ -3,28 +3,15 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+
 router.get('/',(req,res) => {
     res.sendFile( path.join( __dirname + '/index.html' ) );
 });
 
+
 app.use('/',router);
 
+app.use(express.static(__dirname + '/scss'));
+app.use(express.static(__dirname + '/node_modules'));
+
 app.listen(process.env.port || 3000);
-
-// const http = require('http');
-// const hostname ='127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer( 
-//     (req,res) => {
-//         res.statusCode = 200;
-//         res.setHeader('Content-Type','text/plain');
-//         res.end('index.html');
-//     }
-// );
-
-// server.listen( port, hostname, 
-//     () => {
-//         console.log("Servidor rodando!");
-//     }
-// );
