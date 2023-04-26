53ead21ae3a4cc59bc3a2f768e75132f5420e51a
lucamps <lucas.campos2@ufv.br>
2023-04-26 08:40:23 -0300

    Testando desenhar retas e areas

--- scripts/graph.js
+++ scripts/graph.js
@@ -1,6 +1,27 @@
+
+// Recebe uma matriz n x 2 e retorna uma string a ser usada pelo Desmos
+function pointsToString(points){
+    let pointsString = "";
+    let size = points.length;
+    for(let i = 0; i < size-1; i++){
+        pointsString += '(' + points[i][0] + ',' + points[i][1] +  '),';
+    }
+    pointsString += '(' + points[size-1][0] + ',' + points[size-1][1] +  ')';
+    
+    return pointsString;
+}
+
+// Obtendo grafico
 let elt = document.getElementById('graph');
-let options = {zoomButtons:false,expressions:true}
+let options = {zoomButtons:false,expressions:true};
 let calculator = Desmos.GraphingCalculator(elt,options);
 
-let poligStr = '\\polygon((1,2),(7,4),(3,7))'
-calculator.setExpression({id:'polig1',latex:poligStr})
+// Desenhando retas
+calculator.setExpression({id:'reta1',latex:"2x + y = 5"});
+calculator.setExpression({id:'reta',latex:"-8x + 7y = 9"});
+
+// Desenhando um polígono
+let points = [[1,2],[3,7],[7,4],[8,1]];
+let pointsString = pointsToString(points);
+let poligStr = `\\polygon(${pointsString})`;
+calculator.setExpression({id:'polig1',latex:poligStr });
