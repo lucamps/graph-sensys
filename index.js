181a7d5c64068357cddb077bd01b6ab147a29544
lucamps <lucas.campos2@ufv.br>
2023-05-01 12:30:21 -0300

    Obtendo dados do input

--- index.js
+++ index.js
@@ -7,15 +7,22 @@ import * as url from 'url';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const app = express();
-const router = express.Router();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.post("/", (req, res) => {
+    const inputText = req.body.input_label;
+    let ps = new ParserImpl(inputText);
+    console.log("INPUT DATA:");
+    console.log(ps.listener.funcObj);
+    console.log(ps.listener.stList);
+    return;
+});
 
-app.use('/', router);
 
 app.use(express.static(__dirname + '/scss'));
 app.use(express.static(__dirname + '/node_modules'));
@@ -24,11 +31,3 @@ app.use(express.static(__dirname + '/scripts'));
 // eslint-disable-next-line no-undef
 app.listen(process.env.port || 3000);
 
-let input = `max 3u + 2z
-sa
-insumo) 5u + z < 2`;
-// eslint-disable-next-line no-unused-vars
-let ps = new ParserImpl(input);
-
-console.log(ps.listener.funcObj);
-console.log(ps.listener.stList);
