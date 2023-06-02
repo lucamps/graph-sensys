ee300f0d9af38bcb1ebd5e12ede28dcd39647b8c
lucamps <lucas.campos2@ufv.br>
2023-06-02 08:11:35 -0300

    GS-24 iniciando alteracoes, modificando slider

--- scripts/htmlContentHandler.mjs
+++ scripts/htmlContentHandler.mjs
@@ -2,10 +2,8 @@ export default class HtmlContentHandler {
     static getDivRes(rest) {
         const style = `style="border:0.2em solid ${rest.color}"`;
 
-        const divContent = `<div class="alert alert-custom alert-dismissible custom-box fade show" id="div-${rest.id}" ${style} role="alert">
+        const divContent = ` <li class="list-group-item" id="div-${rest.id}" ${style}>
             <p class="identificador">${rest.id}</p>
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"
-                id="close-${rest.id}"></button>
             <form class="form-expression">
                 <div class="form form-res" id="form-${rest.id}">
                     <p id="x-value-${rest.id}">${rest.a}</p>
@@ -17,7 +15,7 @@ export default class HtmlContentHandler {
                 <label for="range-${rest.id}" class="form-label"></label>
                 <input type="range" class="form-range" min="0" max="5" step="0.5" id="range-${rest.id}">
             </form>
-        </div>`;
+        </li>`;
 
         return divContent
     }
